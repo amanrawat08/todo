bd8d1fe04a9470c93502687091dcf1d3b7bae592
amanrawat08 <ar7541147@gmail.com>
2024-07-31 10:45:01 +0530

    navBar

---
+++ todo/src/FrontPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+export function FrontPage() {
+  return (
+    <div className="frontpage">
+      <div className="frontData">
+        <h1>Welcome</h1>
+        <div className="frontTxt">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+          cumque? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex,
+          sunt.
+        </div>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
